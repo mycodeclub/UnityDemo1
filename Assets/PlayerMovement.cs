6078d27074da9cf27d94862cc9dc34d57ae4abef
MyCodeClub_DellPc <ankitsahay1990@gmail.com>
2020-08-06 16:55:35 +0530

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    int speed = 1000;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+      //  rb.AddForce(0,0,speed * Time.deltaTime );
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, speed * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(0, 0, -speed * Time.deltaTime);
+        } 
+    }
+}
